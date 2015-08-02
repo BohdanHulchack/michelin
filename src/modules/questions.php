91e80b4b7cf9af7df1bb49732953c262be139545
BohdanHulchack <major.barberry@gmail.com>
2015-08-03 02:57:03 +0300

    Created Questions module

--- src/modules/questions.php
+++ src/modules/questions.php
@@ -2,23 +2,24 @@
 <section class="questions">
   <h4 class="questions--title">Остались вопросы?</h4>
   <div class="row">
-    <div class="col-3 columns">
+    <div class="col-4 columns">
       <h4 class="questions--subtitle">Телефон менеджера</h4>
       <h5 class="questions--desc">для вопросов и консультации</h5>
-      <div class="questions--manager"><img src="/img/manager.png" alt="" class="questions--manager--image"/>
+      <div class="questions--manager">
+        <div class="questions--manager--image"><img src="/img/manager.png" alt=""/></div>
         <h4 class="questions--manager--name">Константин Озаренский</h4>
         <h5 class="questions--manager--desc">Менеджер по продажам</h5>
-        <div class="icon-ipad questions--manager--tel-img"></div><a href="tel:+3800000000" class="questions--manager--tel">(000) 000-00-00</a>
+        <div class="icon-iphone questions--manager--tel-img"></div><a href="tel:+3800000000" class="questions--manager--tel">(000) 000-00-00</a>
       </div>
     </div>
-    <div class="col-3 columns">
+    <div class="col-4 columns">
       <h4 class="questions--subtitle">Чат оn-line</h4>
-      <h5 class="questions--desc">с консультантом</h5><a href="" class="questions--link">
+      <h5 class="questions--desc">с консультантом</h5><a href="" class="questions--link questions--link__chat">
         <div class="icon-chat questions--link--image"></div><span class="questions--link--btm">Начать чат</span></a>
     </div>
-    <div class="col-3 columns">
+    <div class="col-4 columns">
       <h4 class="questions--subtitle">Перезвоните мне</h4>
-      <h5 class="questions--desc">обратный звонок</h5><a href="" class="questions--link">
+      <h5 class="questions--desc">обратный звонок</h5><a href="" class="questions--link questions--link__tel">
         <div class="icon-ipad questions--link--image"></div><span class="questions--link--btm">Заказать</span></a>
     </div>
   </div>
