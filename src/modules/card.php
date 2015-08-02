6d90d13736d924f42378a223b203a50ea94a07dd
BohdanHulchack <major.barberry@gmail.com>
2015-08-03 02:03:28 +0300

    Created card module

--- src/modules/card.php
+++ src/modules/card.php
@@ -1,9 +1,12 @@
 
 <div class="card">
-  <h3 class="card--name">Michelin Agilis</h3><img src="img/item.png" alt="Michelin Agilis" class="card--img"/>
+  <h3 class="card--name">Michelin Agilis</h3>
+  <div class="card--logo"><img src="img/item_logo.png" alt="ADAC"/></div>
+  <div class="card--img"><img src="img/item.png" alt="Michelin Agilis"/></div>
   <ul class="card--char">
     <li class="card--char--diam">R14</li>
     <li class="card--char--width">185/60</li>
     <li class="card--char--price">1196<i>грн</i></li>
-  </ul><a href="" class="card--more"></a><a href="" class="card--buy"></a>
+  </ul>
+  <div class="card--footer"><a href="" class="card--footer--more">Подробнее</a><a href="" class="card--footer--buy right">Купить</a></div>
 </div>
